92adce844f02e690d8ea78cdbbd82c09e1ae3e93
Bhyland <blakehyland95@gmail.com>
2021-09-30 15:30:04 -0600

    updated constructors

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+
 const buildTeam = () => {
     return inquirer.prompt([
         {
