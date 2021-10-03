e8cdbec1fcd00c775374113720306a17fb36b97a
bhyland <blakehyland95@gmail.com>
2021-10-03 16:33:12 -0600

    setup test functionality and added functions to constructors

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,22 @@
 const Engineer = require('../lib/Engineer');
+
+// creating engineer object  
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('test', 1, 'test@gmail', 'test');
+    
+    expect(engineer.github) .toEqual(expect.any(String));
+});
+
+// gets github from getGithub()
+test('gets engineer github value', () => {
+    const engineer = new Engineer('test', 1, 'test@gmail', 'test');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// gets role from getRole() 
+test('gets role of employee', () => {
+    const engineer = new Engineer('test', 1, 'test@gmail', 'test');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
