4001eef30cffafb8188d62e6f58ef04d7f7e5cb0
bhyland <blakehyland95@gmail.com>
2021-10-03 16:12:36 -0600

    updated generate HTML file to pass in team cards

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,29 +1,85 @@
-const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
+//generateManager
+let generateManager = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.officeNumber}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
 
-generateHTML = (data) =>{
-   return "hello"
+//generateEngineer
+let generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+}
 
-    //generateManager
+//generateIntern
+let generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+    `
+}
 
-    //generateEngineer
-
-    //generateIntern
-   
+generateHTML = data => {
 
     const teamCards = []
-    teamCards.push(data.filter(employee=>employee.getrole()==="Manager").map(manager=>generateManager(manager)))
-    teamCards.push(data.filter(employee=>employee.getrole()==="Intern").map(intern=>generateIntern(intern)))
-    teamCards.push(data.filter(employee=>employee.getrole()==="Engineer").map(engineer=>generateEngineer(engineer)))
 
+    teamCards.push(data.filter(employee => employee.getrole() === "Manager").map(manager => generateManager(manager)))
+    teamCards.push(data.filter(employee => employee.getrole() === "Intern").map(intern => generateIntern(intern)))
+    teamCards.push(data.filter(employee => employee.getrole() === "Engineer").map(engineer => generateEngineer(engineer)))
+
+    console.log(teamCards)
+    // joining strings 
     return teamCards.join("")
+
+
 }
 
 
-// generate html page 
-let generatePage = (data) =>{   
 
-return  `
+
+
+
+
+// generate html page 
+let generatePage = (data) => {
+
+    return `
   <!DOCTYPE html>
   <html lang="en">
   <head>
